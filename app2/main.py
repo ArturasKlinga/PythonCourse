59f3b699b3c313509776bcaf51a56b3ea35e831e
Arturas Klinga <arturas.klinga@panko.lt>
2021-07-14 16:02:33 +0300

    Marker added.

--- app2/main.py
+++ app2/main.py
@@ -1,4 +1,10 @@
 import folium
 
-map = folium.Map(location=[55.73, 24.26])
+map = folium.Map(location=[55.73, 24.35])
+
+fg = folium.FeatureGroup(name="My map")
+fg.add_child(folium.Marker(location=[55.73, 24.35], popup="Marker", icon=folium.Icon(color="green")))
+
+map.add_child(fg)
+
 map.save("Map1.html")
