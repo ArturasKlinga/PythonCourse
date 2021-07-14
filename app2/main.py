be90a669f458e8c1b4b057c8ba33448afda17744
Arturas Klinga <arturas.klinga@panko.lt>
2021-07-14 18:55:43 +0300

    Markers with multiple colors according to volcano elevation.

--- app2/main.py
+++ app2/main.py
@@ -8,12 +8,20 @@ lon = list(data["LON"])
 elev = list(data["ELEV"])
 name = list(data["NAME"])
 
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
 map = folium.Map(location=[38.58, -99.09], zoom_start=6)
 
 fg = folium.FeatureGroup(name="My map")
 
 for lt, ln, el, na in zip(lat, lon, elev, name):
-    fg.add_child(folium.Marker(location=[lt, ln], popup=str(na) + "\n" + str(el) + " m.", icon=folium.Icon(color="green")))
+    fg.add_child(folium.Marker(location=[lt, ln], popup=str(na) + "\n" + str(el) + " m.", icon=folium.Icon(color=color_producer(el))))
 
 map.add_child(fg)
 
