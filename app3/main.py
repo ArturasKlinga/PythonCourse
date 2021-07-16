af36a8055c77e05d8ea5253f73c0f84fe3b062b6
Arturas Klinga <arturas.klinga@panko.lt>
2021-07-16 16:23:55 +0300

    Removing prints in console, using real hosts file now.

--- app3/main.py
+++ app3/main.py
@@ -9,8 +9,7 @@ website_list = ["www.facebook.com", "facebook.com", "www.google.lt", "google.lt"
 
 while True:
     if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
-        print("Working hours.")
-        with open(hosts_temp, 'r+') as file:
+        with open(hosts_path, 'r+') as file:
             content = file.read()
             for website in website_list:
                 if website in content:
@@ -18,12 +17,11 @@ while True:
                 else:
                     file.write(redirect + " " + website + "\n")
     else:
-        with open(hosts_temp, 'r+') as file:
+        with open(hosts_path, 'r+') as file:
             content = file.readlines()
             file.seek(0)
             for line in content:
                 if not any(website in line for website in website_list):
                     file.write(line)
             file.truncate()
-        print("Fun hours.")
     time.sleep(5)
