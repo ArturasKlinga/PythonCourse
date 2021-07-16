bc11637a71fb132a794ffa3996efc730fbda67fc
Arturas Klinga <arturas.klinga@panko.lt>
2021-07-16 14:50:20 +0300

    Polygon color depends on countries population.

--- app2/main.py
+++ app2/main.py
@@ -26,7 +26,9 @@ for lt, ln, el, na in zip(lat, lon, elev, name):
     fg.add_child(folium.CircleMarker(location=[lt, ln], fill=True, radius=6, popup=str(na) + "\n" + str(el) + " m.",
                                      fill_color=color_producer(el), color='grey', fill_opacity=0.7))
 
-fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read()))
+fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+                            style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 10000000
+                            else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
 map.add_child(fg)
 
