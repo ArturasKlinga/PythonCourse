9fa3988d9a8aa44b471195448de7af237e5fb4e1
Arturas Klinga <arturas.klinga@panko.lt>
2021-07-16 15:01:22 +0300

    Added layer control panel.

--- app2/main.py
+++ app2/main.py
@@ -20,16 +20,21 @@ def color_producer(elevation):
 
 map = folium.Map(location=[38.58, -99.09], zoom_start=6)
 
-fg = folium.FeatureGroup(name="My map")
+fgv = folium.FeatureGroup(name="Volcanoes")
 
 for lt, ln, el, na in zip(lat, lon, elev, name):
-    fg.add_child(folium.CircleMarker(location=[lt, ln], fill=True, radius=6, popup=str(na) + "\n" + str(el) + " m.",
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], fill=True, radius=6, popup=str(na) + "\n" + str(el) + " m.",
                                      fill_color=color_producer(el), color='grey', fill_opacity=0.7))
 
-fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+fgp = folium.FeatureGroup(name="Population")
+
+fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
                             style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 10000000
                             else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
-map.add_child(fg)
+
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
 
 map.save("Map1.html")
