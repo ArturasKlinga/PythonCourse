112a034fd1e3ac8d60066f3818c6665c9a972280
Arturas Klinga <arturas.klinga@panko.lt>
2021-07-23 20:04:48 +0300

    Delete and update buttons are now functional.

--- app5/backend.py
+++ app5/backend.py
@@ -4,8 +4,7 @@ import sqlite3
 def connect():
     conn = sqlite3.connect("books.db")
     cur = conn.cursor()
-    cur.execute(
-        "CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title TEXT, author TEXT, year INTEGER, isbn INTEGER)")
+    cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title TEXT, author TEXT, year INTEGER, isbn INTEGER)")
     conn.commit()
     conn.close()
 
@@ -49,10 +48,9 @@ def delete(id):
 def update(id, title, author, year, isbn):
     conn = sqlite3.connect("books.db")
     cur = conn.cursor()
-    cur.execute("UPDATE book SET title=?, author=?, year=?, isbn=? WHERE id=?", (id, title, author, year, isbn))
+    cur.execute("UPDATE book SET title=?, author=?, year=?, isbn=? WHERE id=?", (title, author, year, isbn, id))
     conn.commit()
     conn.close()
 
 
 connect()
-print(view())
