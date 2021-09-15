749a69dbeeef6226c9033f08c21f50fa8eb0b22e
Arturas Klinga <arturas.klinga@panko.lt>
2021-09-15 22:16:00 +0300

    Added button to download table values in csv file.

--- app10/app.py
+++ app10/app.py
@@ -18,14 +18,14 @@ def success_table():
         df["coordinates"] = df["Address"].apply(gc.geocode)
         df["Latitude"] = df["coordinates"].apply(lambda x: x.latitude if x != None else None)
         df["Longitude"] = df["coordinates"].apply(lambda x: x.latitude if x != None else None)
-        df = df.drop("coordinates", 1)
+        df = df.drop(columns="coordinates")
         df.to_csv("uploads/geocoded.csv", index=None)
-        return render_template("index.html", text=df.to_html())
+        return render_template("index.html", text=df.to_html(), btn="download.html")
 
 
 @app.route("/download-file/")
 def download():
-    return send_file()
+    return send_file("uploads/geocoded.csv", download_name="yourfile.csv", as_attachment=True)
 
 
 if __name__ == '__main__':
