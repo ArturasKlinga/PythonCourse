54c8a600934649f11d3d841e027fd1c2a00ff42a
Arturas Klinga <arturas.klinga@panko.lt>
2021-09-15 23:19:10 +0300

    Giving user a message if error happens while working with csv file.

--- app10/app.py
+++ app10/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, send_file
 import pandas
 from geopy.geocoders import Nominatim
+
 app = Flask(__name__)
 
 
@@ -13,14 +14,17 @@ def index():
 def success_table():
     if request.method == "POST":
         file = request.files['file']
-        df = pandas.read_csv(file)
-        gc = Nominatim(user_agent="http")
-        df["coordinates"] = df["Address"].apply(gc.geocode)
-        df["Latitude"] = df["coordinates"].apply(lambda x: x.latitude if x != None else None)
-        df["Longitude"] = df["coordinates"].apply(lambda x: x.latitude if x != None else None)
-        df = df.drop(columns="coordinates")
-        df.to_csv("uploads/geocoded.csv", index=None)
-        return render_template("index.html", text=df.to_html(), btn="download.html")
+        try:
+            df = pandas.read_csv(file)
+            gc = Nominatim(user_agent="http")
+            df["coordinates"] = df["Address"].apply(gc.geocode)
+            df["Latitude"] = df["coordinates"].apply(lambda x: x.latitude if x != None else None)
+            df["Longitude"] = df["coordinates"].apply(lambda x: x.latitude if x != None else None)
+            df = df.drop(columns="coordinates")
+            df.to_csv("uploads/geocoded.csv", index=None)
+            return render_template("index.html", text=df.to_html(), btn="download.html")
+        except:
+            return render_template("index.html", text="Please make sure you have an address column in your CSV file!")
 
 
 @app.route("/download-file/")
