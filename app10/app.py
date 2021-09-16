1700f4a93dc0bed1abaeeca845221a3e204abbbc
Arturas Klinga <arturas.klinga@panko.lt>
2021-09-16 19:36:34 +0300

    Uploaded csv file gets unique name in backend.

--- app10/app.py
+++ app10/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, send_file
 import pandas
 from geopy.geocoders import Nominatim
+import datetime
 
 app = Flask(__name__)
 
@@ -12,6 +13,7 @@ def index():
 
 @app.route("/success-table", methods=['POST'])
 def success_table():
+    global filename
     if request.method == "POST":
         file = request.files['file']
         try:
@@ -21,7 +23,8 @@ def success_table():
             df["Latitude"] = df["coordinates"].apply(lambda x: x.latitude if x != None else None)
             df["Longitude"] = df["coordinates"].apply(lambda x: x.latitude if x != None else None)
             df = df.drop(columns="coordinates")
-            df.to_csv("uploads/geocoded.csv", index=None)
+            filename = datetime.datetime.now().strftime("uploads/%Y-%m-%d-%H-%M-%S-%f" + ".csv")
+            df.to_csv(filename, index=None)
             return render_template("index.html", text=df.to_html(), btn="download.html")
         except:
             return render_template("index.html", text="Please make sure you have an address column in your CSV file!")
@@ -29,7 +32,7 @@ def success_table():
 
 @app.route("/download-file/")
 def download():
-    return send_file("uploads/geocoded.csv", download_name="yourfile.csv", as_attachment=True)
+    return send_file(filename, download_name="yourfile.csv", as_attachment=True)
 
 
 if __name__ == '__main__':
